afd0b382098346f5293f862ae34e851e20f75363
DanteEniyel <eni.gitmail@gmail.com>
2014-02-26 00:27:23 +0100

    Fourth commit

---
+++ pomocna_aplikacija_jelvalcic_1/pomocna_aplikacija_jelvalcic_1/src/pomocna_aplikacija_jelvalcic_1/Pomocna_aplikacija_jelvalcic_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pomocna_aplikacija_jelvalcic_1;
+
+/**
+ *
+ * @author jelvalcic
+ */
+public class Pomocna_aplikacija_jelvalcic_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GuiSlanjeKomandi gui = new GuiSlanjeKomandi();
+        gui.setVisible(true);
+    }
+}
